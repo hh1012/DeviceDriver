877eb30e6f4cfd6dc4896b05208257ce1d6bf873
hoon.hur <hurhoon1012@gmail.com>
2025-07-24 16:01:39 +0900

    [feature] device driver read

--- DeviceDriver/device_driver.cpp
+++ DeviceDriver/device_driver.cpp
@@ -6,8 +6,15 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int ret = (int)(m_hardware->read(address));
+    int tmp;
+    for (int i = 0; i < 4; i++) {
+        tmp = (int)m_hardware->read(address);
+        if (ret != tmp) {
+            throw ReadFailException();
+        }
+    }
+    return ret;
 }
 
 void DeviceDriver::write(long address, int data)
@@ -15,3 +22,4 @@ void DeviceDriver::write(long address, int data)
     // TODO: implement this method
     m_hardware->write(address, (unsigned char)data);
 }
+
