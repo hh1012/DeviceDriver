e9bde1af89b4231f953278bb28f9e93a28fcddb7
hoon.hur <hurhoon1012@gmail.com>
2025-07-24 16:01:39 +0900

    final

--- DeviceDriver/main.cpp
+++ DeviceDriver/main.cpp
@@ -5,88 +5,86 @@ using namespace testing;
 
 class FlashMock : public FlashMemoryDevice {
 public:
-	MOCK_METHOD(unsigned char, read, (long address), (override));
-	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+    MOCK_METHOD(unsigned char, read, (long address), (override));
+    MOCK_METHOD(void, write, (long address, unsigned char data), (override));
 };
 
-class DeviceDriverFixture : public Test {
-public:
-	FlashMock flashMock;
-	DeviceDriver driver{ &flashMock };
-};
+TEST(DeviceDriverRead, FiveRead) {
+    FlashMock mock;
 
-TEST_F(DeviceDriverFixture, FiveRead) {
-	// TODO : replace hardware with a Test Double
+    EXPECT_CALL(mock, read((long)0xB))
+        .Times(5);
 
-	EXPECT_CALL(flashMock, read((long)0xFF))
-		.Times(5);
-	
-	driver.read((long)0xFF);
+    DeviceDriver driver{ &mock };
+    int data = driver.read((long)0xB);
 }
 
-TEST_F(DeviceDriverFixture, FiveReadWithSameValue) {
-	// TODO : replace hardware with a Test Double
+TEST(DeviceDriverRead, ReadAndException) {
+    FlashMock mock;
 
-	unsigned char expected = 0x1;
-	EXPECT_CALL(flashMock, read((long)0xFF))
-		.Times(5)
-		.WillRepeatedly(Return(expected));
+    EXPECT_CALL(mock, read((long)0xB))
+        .WillOnce(Return((unsigned char)0xA))
+        .WillOnce(Return((unsigned char)0xA))
+        .WillOnce(Return((unsigned char)0xA))
+        .WillOnce(Return((unsigned char)0xA))
+        .WillOnce(Return((unsigned char)0xB));
 
-	unsigned char actual = driver.read((long)0xFF);
-	EXPECT_EQ(expected, actual);
+    DeviceDriver driver{ &mock };
+    EXPECT_THROW(driver.read((long)0xB), std::exception);
 }
 
-TEST_F(DeviceDriverFixture, FiveReadWithDifferentValue) {
-	// TODO : replace hardware with a Test Double
+TEST(DeviceDriverWrite, ReadBeforeWrite) {
+    NiceMock<FlashMock> mock;
 
-	unsigned char expected1 = 0x1;
-	unsigned char expected2 = 0x2;
-	unsigned char expected3 = 0x3;
-	unsigned char expected4 = 0x4;
-	unsigned char expected5 = 0x5;
-	EXPECT_CALL(flashMock, read((long)0xFF))
-		.Times(AtMost(5))
-		.WillOnce(Return(expected1))
-		.WillOnce(Return(expected2))
-		.WillOnce(Return(expected3))
-		.WillOnce(Return(expected4))
-		.WillOnce(Return(expected5));
+    EXPECT_CALL(mock, read((long)0xB))
+        .Times(1)
+        .WillRepeatedly(Return((unsigned char)0xFF));
 
-	EXPECT_THROW(driver.read((long)0xFF), ReadFailException);
+    DeviceDriver driver{ &mock };
+    driver.write((long)0xB, 7);
+}
+TEST(DeviceDriverWrite, WriteException) {
+    NiceMock<FlashMock> mock;
+
+    EXPECT_CALL(mock, read((long)0xB))
+        .WillRepeatedly(Return((unsigned char)0xFE));
+
+    DeviceDriver driver{ &mock };
+    EXPECT_THROW(driver.write((long)0xB, 7), std::exception);
 }
 
-TEST_F(DeviceDriverFixture, ReadBeforeWrite) {
-	long address = 0x1;
-	unsigned char data = 0x2;
-	EXPECT_CALL(flashMock, read(address))
-		.Times(1)
-		.WillRepeatedly(Return((unsigned char)0xFF));
-	driver.write(address, (int)data);
+
+TEST(Application, ReadAndPrint) {
+    NiceMock<FlashMock> mock;
+    DeviceDriver dd{ &mock };
+    Application app{ &dd };
+
+    EXPECT_CALL(mock, read)
+        .Times(25);
+
+    //콘솔출력 될것
+    app.readAndprint(0x00, 0x04);
+
+    //1. 안한다. 
+    //    내부적으로 랩퍼 클래스 반환값 Proxy 출력
+    //2. 콘솔 버퍼 옮겼다가 다시 원복 
+         //Trivia KATA
+    //3. 행동검증
 }
 
-TEST_F(DeviceDriverFixture, WriteSuccess) {
-	long address = 0x1;
-	unsigned char data = 0x2;
-	EXPECT_CALL(flashMock, read(address))
-		.Times(1)
-		.WillRepeatedly(Return((unsigned char)0xFF));
-	EXPECT_CALL(flashMock, write(address, data))
-		.Times(1);
-	driver.write(address, (int)data);
-}
+TEST(Application, WriteAll) {
+    NiceMock<FlashMock> mock;
+    DeviceDriver dd{ &mock };
+    Application app{ &dd };
 
-TEST_F(DeviceDriverFixture, WriteFail) {
-	long address = 0x1;
-	unsigned char data = 0x2;
-	EXPECT_CALL(flashMock, read(address))
-		.Times(1)
-		.WillRepeatedly(Return((unsigned char)0x1));
-	EXPECT_CALL(flashMock, write(address, data))
-		.Times(0);
-	EXPECT_THROW(driver.write(address, (int)data), std::exception);
+    EXPECT_CALL(mock, read)
+        .Times(5)
+        .WillRepeatedly(Return((unsigned char)0xFF));
+
+    app.writeAll(0x77);
 }
 
 int main() {
-	::testing::InitGoogleMock();
-	return RUN_ALL_TESTS();
+    ::testing::InitGoogleMock();
+    return RUN_ALL_TESTS();
 }
